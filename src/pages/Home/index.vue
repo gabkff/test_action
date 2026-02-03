82fa728ceee10bb89e279412e2953b9b2f59339e
gabriel <gabriel.corbel@kffein.com>
2026-02-03 11:45:40 -0500

    remove unused font and fix style home

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -3,24 +3,6 @@
     <div class="home__part">
       <div class="bg bg_fr" :style="{backgroundImage: `url(${ImgBackgroundFr})`}"></div>
       <div class="home__part__wrapper" ref="wrapperFrRef" :style="wrapperFrMaskStyle">
-        <svg class="wrapper-mask-svg" aria-hidden="true">
-          <defs>
-            <mask id="wrapper-mask-fr" maskUnits="objectBoundingBox" maskContentUnits="objectBoundingBox">
-              <rect x="0" y="0" width="1" height="1" fill="white"/>
-              <rect
-                v-for="(h, i) in holeRectsFr"
-                :key="i"
-                :x="h.x"
-                :y="h.y"
-                :width="h.w"
-                :height="h.h"
-                rx="0.02"
-                ry="0.02"
-                fill="black"
-              />
-            </mask>
-          </defs>
-        </svg>
         <div class="line_1_fr line">
           <div class="line_1_fr__title text_fr"> LAISSEZ</div>
           <div class="mask mask1_fr"></div>
@@ -29,11 +11,13 @@
           <div class="line_2_fr__title text_fr"> VOUS</div>
           <div class="mask mask2_fr"></div>
         </div>
-        <div class="line_3_fr mask line"></div>
+        <div class="line_3_fr line">
+          <div class="mask mask3_fr"></div>
+        </div>
         <div class="line_4_fr line">
           <div class="mask mask4_fr"></div>
           <div class="line_4_fr__title text_fr"> GUIDER</div>
-          <ui-button theme="primary" :label="$t('home.touch_to_start')" @click="router.push('/select')" :icon="IconArrow" :big="true" :iconPosition="'right'"/>
+          <ui-button class="button_fr" theme="primary" :label="'touchez pour commencer'" @click="goSelect('fr')" :icon="IconArrow" :big="true" :iconPosition="'right'"/>
         </div>
       </div>
     </div>
@@ -51,28 +35,44 @@
         <div class="line line_3_en">
           <div class="mask mask3_en"></div>
           <div class="line_3_en__title text_en"> YOU</div>
-          <ui-button theme="primary" :label="$t('home.touch_to_start')" @click="router.push('/select')" :icon="IconArrow" :big="true" :iconPosition="'right'"/>
+          <ui-button class="button_en" theme="primary" :label="'touch to start'" @click="goSelect('en')" :icon="IconArrow" :big="true" :iconPosition="'right'"/>
         </div>
       </div>
-      <svg class="wrapper-mask-svg" aria-hidden="true">
-        <defs>
-          <mask id="wrapper-mask-en" maskUnits="objectBoundingBox" maskContentUnits="objectBoundingBox">
-            <rect x="0" y="0" width="1" height="1" fill="white"/>
-            <rect
-              v-for="(h, i) in holeRectsEn"
-              :key="i"
-              :x="h.x"
-              :y="h.y"
-              :width="h.w"
-              :height="h.h"
-              rx="0.02"
-              ry="0.02"
-              fill="black"
-            />
-          </mask>
-        </defs>
-      </svg>
     </div>
+    
+    <!-- SVG masks inline dans le DOM -->
+    <svg class="wrapper-mask-svg" aria-hidden="true">
+      <defs>
+        <mask id="wrapper-mask-fr" maskUnits="objectBoundingBox" maskContentUnits="objectBoundingBox">
+          <rect x="0" y="0" width="1" height="1" fill="white"/>
+          <rect
+            v-for="(h, i) in holeRectsFr"
+            :key="i"
+            :x="h.x"
+            :y="h.y"
+            :width="h.w"
+            :height="h.h"
+            rx="0.02"
+            ry="0.02"
+            fill="black"
+          />
+        </mask>
+        <mask id="wrapper-mask-en" maskUnits="objectBoundingBox" maskContentUnits="objectBoundingBox">
+          <rect x="0" y="0" width="1" height="1" fill="white"/>
+          <rect
+            v-for="(h, i) in holeRectsEn"
+            :key="i"
+            :x="h.x"
+            :y="h.y"
+            :width="h.w"
+            :height="h.h"
+            rx="0.02"
+            ry="0.02"
+            fill="black"
+          />
+        </mask>
+      </defs>
+    </svg>
   </div>
 </template>
 
@@ -80,9 +80,11 @@
   import IconArrow from 'assets/svg/arrow.svg?raw'
   import ImgBackgroundFr from './assets/back_fr.png'
   import ImgBackgroundEn from './assets/back_en.png'
+  import { useI18nStore } from 'plugins/i18n/store'
   import { useRouter } from 'vue-router'
   import { ref, computed, onMounted, onUnmounted, nextTick } from 'vue'
 
+  const i18nStore = useI18nStore()
   const router = useRouter()
   const wrapperFrRef = ref<HTMLElement | null>(null)
   const overlayEnRef = ref<HTMLElement | null>(null)
@@ -91,20 +93,18 @@
   type HoleRect = { x: number; y: number; w: number; h: number }
   const holeRectsFr = ref<HoleRect[]>([])
   const holeRectsEn = ref<HoleRect[]>([])
+  const holesReady = ref(false)
 
   const maskStyle = (id: string) => ({
     maskImage: `url(#${id})`,
     WebkitMaskImage: `url(#${id})`,
-    maskSize: '100% 100%',
-    WebkitMaskSize: '100% 100%',
-    maskRepeat: 'no-repeat',
-    WebkitMaskRepeat: 'no-repeat',
   })
+  
   const wrapperFrMaskStyle = computed(() =>
-    holeRectsFr.value.length ? maskStyle('wrapper-mask-fr') : {},
+    holesReady.value && holeRectsFr.value.length ? maskStyle('wrapper-mask-fr') : {},
   )
   const overlayEnMaskStyle = computed(() =>
-    holeRectsEn.value.length ? maskStyle('wrapper-mask-en') : {},
+    holesReady.value && holeRectsEn.value.length ? maskStyle('wrapper-mask-en') : {},
   )
 
   function getHoleRects(container: HTMLElement): HoleRect[] {
@@ -122,6 +122,7 @@
   }
 
   function updateHoles() {
+    console.log('updateHoles')
     if (wrapperFrRef.value) holeRectsFr.value = getHoleRects(wrapperFrRef.value)
     if (overlayEnRef.value && partEnRef.value) {
       const partRect = partEnRef.value.getBoundingClientRect()
@@ -136,13 +137,30 @@
         }
       })
     }
+    holesReady.value = true
+  }
+
+  function goSelect(lang: string) {
+    console.log('goEnglish')
+    i18nStore.setLocale(lang as LocaleKey)
+    console.log('i18nStore', i18nStore.locale)
+    router.push('/selection')
   }
 
   onMounted(() => {
-    nextTick(updateHoles)
+    nextTick(() => {
+      updateHoles()
+      // Backup update après un délai
+      setTimeout(updateHoles, 100)
+    })
+    
+    // Recalculer lors du resize
     window.addEventListener('resize', updateHoles)
   })
-  onUnmounted(() => window.removeEventListener('resize', updateHoles))
+  
+  onUnmounted(() => {
+    window.removeEventListener('resize', updateHoles)
+  })
 </script>
 
 <style lang="stylus" scoped>
@@ -152,60 +170,107 @@
   r(gap, 183px)
   width 100vw
   height 100vh
-  r(padding-top, 696px)
+  r(padding-top, 600px)
   r(padding-left, 271px)
   r(padding-right, 271px)
   r(padding-bottom, 656px)
   background-size cover
+  position relative
+  
+  :deep(.UiButton)
+    r(width, 401px)
+    r(height, 312px)
+    margin-top auto
+    margin-bottom auto
+    text-align left
+    
+  .button_fr
+    background $fleuve
+    
+  .button_en
+    background $bouleau
+    
   .home__part
     width 100%
     height 100%
     position relative
+    
   .home__part__wrapper
     background-color $fjord
     position relative
     z-index 1
-    .wrapper-mask-svg
-      position absolute
-      width 0
-      height 0
-      overflow hidden
-    .mask
-      visibility hidden
+    width 100%
+    height 100%
+    transform scale(1.003)
+    f(column, $align: flex-start, $justify: flex-start)
+    r(gap, 30px)
+    
+    
   .home__part__overlay
     position absolute
-    inset 0
+    top 0
+    left 0
+    width 100%
+    height 100%
     z-index 1
+    transform scale(1.004)
     background-color $fjord
     pointer-events none
+    
   .home__part__content
     position relative
     z-index 2
-  .home__part:last-child .wrapper-mask-svg
+    width 100%
+    height 100%
+    f(column, $align: flex-start, $justify: flex-start)
+    r(gap, 30px)
+    
+  .bg
+    position absolute
+    top 0
+    left 0
+    width 100%
+    height 100%
+    z-index 0
+    background-size cover
+    background-position center
+    
+  .wrapper-mask-svg
     position absolute
     width 0
     height 0
     overflow hidden
-  .home__part:last-child .mask
-    visibility hidden
-  .bg
-    position absolute
-    inset 0
-    z-index 0
-    background-size cover
-    background-position center
+    pointer-events none
+    
   .text_fr
     f-style('h0')
     color $ecume
+    
   .text_en
     f-style('h0')
     color $lueur
+    
   .line
-    height 312px
-    f(row, $justify: flex-start, $align: stretch)
+    width 100%
+    flex 1
+    f(row, $justify: flex-start, $align: center)
+    r(gap, 30px)
+    r(margin-top, -40px)
+    r(margin-bottom, -40px)
+    
+  .line:first-child
+    margin-top 0
+    
+  .line:last-child
+    margin-bottom 0
+    
+  .line_3_fr
+    r(min-height, 433px)
+    
   .mask
     flex 1
     min-width 0
-    height 100%
+    r(height, 312px)
     border-radius 20px
+    visibility hidden
 </style>
